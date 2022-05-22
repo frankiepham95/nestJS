d4429207dba0443c6f8789915c4691aee1d3c089
frankiepham95 <frankiepham95@gmail.com>
2022-05-22 17:49:36 +0700

    init project

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,24 @@
+import { TasksService } from './tasks.service';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') taskId: string): Task {
+    return this.tasksService.getTaskById(taskId);
+  }
+
+  @Post()
+  createTasks(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTaskById(id: string): Task {
+    const found = this.tasks.find((a) => a.id === id);
+    if (!found) {
+      throw new NotFoundException(`Task with id "${id}" not found`);
+    }
+    return found;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const task: Task = {
+      id: uuid(),
+      title: createTaskDto.title,
+      description: createTaskDto.description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+
+  deleteTask(taskId: string) {}
+}
